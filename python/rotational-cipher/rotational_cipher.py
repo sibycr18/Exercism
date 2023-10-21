d6aa7b1b63a716c1429add3d815517bde5a45904
Siby C R <sibycr18@gmail.com>
2023-10-21 22:53:18 +0530

    lost track of day tbh

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,2 +1,11 @@
 def rotate(text, key):
-    pass
+    new_text = ""
+    for char in text:
+        if char.isalpha():
+            if char.isupper():
+                new_text += chr(((ord(char) + key - 65) % 26) + 65)
+            else:
+                new_text += chr(((ord(char) + key - 97) % 26) + 97)
+        else:
+            new_text += char
+    return new_text
